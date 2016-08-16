47fcb3029999bf4f3da0a6982265116d6ca6a271
Dave Fehling <dfehling@gmail.com>
2016-08-16 19:37:45 -0400

    fixed data ntuple script

--- Treemaker/test/data.sh
+++ Treemaker/test/data.sh
@@ -5,10 +5,10 @@ num_jobs=20
 for i in `seq $num_jobs`;
 do 
 
-python run_top_xs_TreeMaker.py --dirs='/uscms_data/d3/dfehling/NTUPLES/June/dataA/' --outfile=output/data_A_sec_${i} --sec=${i} --totalSec=$num_jobs &
-python run_top_xs_TreeMaker.py --dirs='/uscms_data/d3/dfehling/NTUPLES/June/dataB/' --outfile=output/data_B_sec_${i} --sec=${i} --totalSec=$num_jobs &
-python run_top_xs_TreeMaker.py --dirs='/uscms_data/d3/dfehling/NTUPLES/June/dataC/' --outfile=output/data_C_sec_${i} --sec=${i} --totalSec=$num_jobs &
-python run_top_xs_TreeMaker.py --dirs='/uscms_data/d3/dfehling/NTUPLES/June/dataD/' --outfile=output/data_D_sec_${i} --sec=${i} --totalSec=$num_jobs &
+python run_top_xs_TreeMaker.py --dirs='/uscms_data/d3/dfehling/NTUPLES/dataA/' --outfile=output/data_A_sec_${i} --sec=${i} --totalSec=$num_jobs &
+python run_top_xs_TreeMaker.py --dirs='/uscms_data/d3/dfehling/NTUPLES/dataB/' --outfile=output/data_B_sec_${i} --sec=${i} --totalSec=$num_jobs &
+python run_top_xs_TreeMaker.py --dirs='/uscms_data/d3/dfehling/NTUPLES/dataC/' --outfile=output/data_C_sec_${i} --sec=${i} --totalSec=$num_jobs &
+python run_top_xs_TreeMaker.py --dirs='/uscms_data/d3/dfehling/NTUPLES/dataD/' --outfile=output/data_D_sec_${i} --sec=${i} --totalSec=$num_jobs &
 
 done
 exit
