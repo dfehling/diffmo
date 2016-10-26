ea9f9a658bb183779eab01f311e0b89566855a7c
Dave Fehling <dfehling@gmail.com>
2016-10-26 14:29:19 -0400

    now removes commands.cmd as well as listofjobs.txt

--- Treemaker/test/ttjets_muon.sh
+++ Treemaker/test/ttjets_muon.sh
@@ -3,6 +3,7 @@
 num_jobs=25
 
 rm listofjobs.txt
+rm commands.cmd
 tar czvfh tarball.tgz ../CONDOR/* ../run_top_xs_TreeMaker.py ../top_xs_TreeMaker*.py
 
 for i in `seq $num_jobs`;
@@ -12,9 +13,9 @@ echo python ./tardir/run_top_xs_TreeMaker.py --dirs=/uscms_data/d3/dfehling/NTUP
 
 echo python ./tardir/run_top_xs_TreeMaker.py --dirs=/uscmst1b_scratch/lpc1/3DayLifetime/dfehling/mcatnlo/  --outfile=ttjets_mcatnlo_muon_noTrig_noPU_sec_${i}"  "--config=muon --isMC=1 --includeTrigger=0 --includePileup=0 --totalSec=$num_jobs --sec=${i} --isMCatNLO=1 >> listofjobs.txt
 
-echo python ./tardir/run_top_xs_TreeMaker.py --dirs=/uscms_data/d3/dfehling/NTUPLES/ttjets/"               "--outfile=ttjets_powhegNC_muon_Trig_PU_sec_${i}"     "--config=muon --isMC=1 --includeTrigger=1 --includePileup=1 --totalSec=$num_jobs --sec=${i} --triggerFile=./tardir/temp --pileupFile=./tardir/muon_pu_100 >> listofjobs.txt
+# echo python ./tardir/run_top_xs_TreeMaker.py --dirs=/uscms_data/d3/dfehling/NTUPLES/ttjets/"               "--outfile=ttjets_powhegNC_muon_Trig_PU_sec_${i}"     "--config=muon --isMC=1 --includeTrigger=1 --includePileup=1 --totalSec=$num_jobs --sec=${i} --triggerFile=./tardir/temp --pileupFile=./tardir/muon_pu_100 >> listofjobs.txt
 
-echo python ./tardir/run_top_xs_TreeMaker.py --dirs=/uscmst1b_scratch/lpc1/3DayLifetime/dfehling/mcatnlo/  --outfile=ttjets_mcatnlo_muon_Trig_PU_sec_${i}"      "--config=muon --isMC=1 --includeTrigger=1 --includePileup=1 --totalSec=$num_jobs --sec=${i} --triggerFile=./tardir/temp --pileupFile=./tardir/muon_pu_100 --isMCatNLO=1 >> listofjobs.txt
+# echo python ./tardir/run_top_xs_TreeMaker.py --dirs=/uscmst1b_scratch/lpc1/3DayLifetime/dfehling/mcatnlo/  --outfile=ttjets_mcatnlo_muon_Trig_PU_sec_${i}"      "--config=muon --isMC=1 --includeTrigger=1 --includePileup=1 --totalSec=$num_jobs --sec=${i} --triggerFile=./tardir/temp --pileupFile=./tardir/muon_pu_100 --isMCatNLO=1 >> listofjobs.txt
 
 done
 
